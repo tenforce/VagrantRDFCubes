50c7662e560dcd69e7e3069c9feb6be1cd156426
pma <paul.massey@tenforce.com>
2015-04-10 12:02:05 +0200

    Updates to the bootstrap.sh, basics, etc.

--- Vagrantfile
+++ Vagrantfile
@@ -12,7 +12,7 @@ Vagrant.configure(2) do |config|
 
   # Every Vagrant development environment requires a box. You can search for
   # boxes at https://atlas.hashicorp.com/search.
-  config.vm.box = "deb/wheezy-amd64"
+  config.vm.box = "hashicorp/precise64"
   config.vm.provision :shell, path: "bootstrap.sh"
 
   # Disable automatic box update checking. If you disable this, then
@@ -45,7 +45,7 @@ Vagrant.configure(2) do |config|
   # Example for VirtualBox:
   #
   config.vm.provider "virtualbox" do |vb|
-      vb.name = "UnifiedView1.5.5-wheezy"
+      vb.name = "RDFDataCubesVagrantUbuntuPrecise"
       vb.gui = true
       vb.customize ["modifyvm", :id, "--memory", 4096]
       vb.customize ["modifyvm", :id, "--vram", 64]
--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,7 @@ apt-get install -y gnome-shell
 apt-get install -y xterm
 apt-get install -y gnome-terminal
 apt-get install -y gdm3
+apt-get install -y swiprolog
 dpkg-reconfigure gdm3
 
 # Now start to setup for building unified views, etc.
@@ -40,6 +41,10 @@ apt-get install -y debconf-utils
 
 apt-get install -y dpkg-dev build-essential
 apt-get install -y quilt gdebi
+
+# Setup for expanding the source lists
+echo "deb http://stack.linkeddata.org/deb/ ldstack main" >> /etc/apt/sources.list
+apt-get update upgrade
 
 ###############################################################
 # Pull the virtuoso opensource latest package
@@ -77,51 +82,12 @@ echo "unifiedviews-backend-mysql      backend/mysql_db_user string uv"| debconf-
 ##############################################################
 # DB installations, etc which have customised values.
 
+apt-get install -y lod2statworkbench
 apt-get install -y mysql-server-5.5 mysql-server
 apt-get install -y virtuoso-opensource
 apt-get install -y iceweasel
 apt-get update
 
-###############################################################
-# Unified views pulling, packaging and hopefully the installation.
-#
-cd ${HOME}
-if [ -d Packages ]
-then
-    cd Packages ; git pull
-else
-    git clone --branch  UV_v1.5.5 https://github.com/UnifiedViews/Packages.git
-fi
-cd ${HOME}/Packages
-mvn package
-cd target
-cp ${HOME}/Packages/*/target/*.deb .
-echo "****** packages built - installing"
-dpkg -i unifiedviews-*mysql*1.5.5_all.deb unifiedviews-backend-shared-1.5.5_all.deb unifiedviews-backend-1.5.5_all.deb unifiedviews-webapp_1.5.5_all.deb unifiedviews-webapp-shared_1.5.5_all.deb
-apt-get -f -y install 
-
-###############################################################
-# Include the plugins?
-echo "****** unified views installed - build plugins"
-
-cd ${HOME}
-if [ -d Plugins ]
-then
-   cd Plugins ; git pull
-else
-       git clone --branch UV_Plugins_v1.5.5 https://github.com/UnifiedViews/Plugins.git
-fi
-cd ${HOME}/Plugins
-mvn install -Dprofile=extract-jars
-echo "****** copy all plugins"
-cp -fr /var/lib/unifiedviews/target/dpu /var/lib/unifiedviews/target/dpu.old
-for i in e* t* l* ; do mkdir -p /var/lib/unifiedviews/target/dpu/uv-$i; cp $i/target/uv-$i*.jar /var/lib/unifiedviews/target/dpu/uv-$i; done
-# Remove duplicate line??
-sed -e '24d'  plugins.sql | mysql -uroot -proot unifiedviews
-
-###############################################################
-# Setup other services
-update-rc.d unifiedviews-backend defaults
 # Allows login without password
 echo "vagrant ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/vagrant
 echo "****** done with bootstrap"
